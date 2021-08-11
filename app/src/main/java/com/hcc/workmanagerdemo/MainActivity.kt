f7bb74b9bbc4c41c246581bab02571acb3ceae47
hecuncun <hecuncun@moqing.com>
2021-08-11 13:35:01 +0800

    基础用法

--- app/src/main/java/com/hcc/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/hcc/workmanagerdemo/MainActivity.kt
@@ -2,12 +2,17 @@ package com.hcc.workmanagerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.work.*
 import com.hcc.workmanagerdemo.databinding.ActivityMainBinding
-
+const val INPUT_DATA_KEY = "input_data_key"
+const val OUTPUT_DATA_KEY = "output_data_key"
+const val WORK_A_NAME = "A"
+const val WORK_B_NAME = "B"
 class MainActivity : AppCompatActivity() {
-    private val workerManager = WorkManager.getInstance(this)
+    //初始化workerManager
+    private val workerManager =WorkManager.getInstance(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +25,29 @@ class MainActivity : AppCompatActivity() {
     private fun bindView(activityMainBinding: ActivityMainBinding) {
         with(activityMainBinding){
             button.setOnClickListener {
-                val workRequest = OneTimeWorkRequestBuilder<MyWorker>()
+                //为worker 添加执行约束
+                val constraints = Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.CONNECTED)
                     .build()
-                workerManager.enqueue(workRequest)
+                //创建workRequest  加入到workerManager队列
+                val workRequestA = OneTimeWorkRequestBuilder<MyWorker>()
+                    .setConstraints(constraints)//设置约束
+                    .setInputData(workDataOf(INPUT_DATA_KEY to WORK_A_NAME))//传入一个数据  键值对形式
+                    .build()
+                val workRequestB = OneTimeWorkRequestBuilder<MyWorker>()
+                    .setConstraints(constraints)//设置约束
+                    .setInputData(workDataOf(INPUT_DATA_KEY to WORK_B_NAME))//传入一个数据  键值对形式
+                    .build()
+                //执行链式任务
+                workerManager.beginWith(workRequestA).then(workRequestB).enqueue()
+                //监听执行结果输出
+                workerManager.getWorkInfoByIdLiveData(workRequestA.id).observe(this@MainActivity, Observer {
+                    Log.d("work","onCreate ${it.state}")
+                    if (it.state == WorkInfo.State.SUCCEEDED){
+                        Log.d("work","onCreate ${it.outputData.getString(OUTPUT_DATA_KEY)}")
+                    }
+
+                })
             }
         }
     }
--- app/src/main/java/com/hcc/workmanagerdemo/MyWorker.kt
+++ app/src/main/java/com/hcc/workmanagerdemo/MyWorker.kt
@@ -4,15 +4,17 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 
 /**
  * Created by hecuncun on 2021/8/10
  */
 class MyWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
     override fun doWork(): Result {
-        Log.d("MyWorker", "doWork:start ")
+        val name =  inputData.getString(INPUT_DATA_KEY)
+        Log.d("MyWorker", "doWork:${name}start ")
         Thread.sleep(3000)
-        Log.d("MyWorker", "doWork:end ")
-        return Result.success()
+        Log.d("MyWorker", "doWork:${name}end ")
+        return Result.success(workDataOf(OUTPUT_DATA_KEY to "$name output"))
     }
 }
